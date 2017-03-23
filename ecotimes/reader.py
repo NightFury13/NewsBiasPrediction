c0c8dd7d3cc0035a3d487bf47cbf4a01c968f3c6
NightFury13 <develop13mohit@gmail.com>
2017-03-23 14:09:23 +0530

    adding data after 30k iterations

--- ecotimes/reader.py
+++ ecotimes/reader.py
@@ -1,11 +1,16 @@
 import pickle as pkl
 
-in_file = '10000.0_eco_data.pkl'
+in_file = '30000.0_eco_data.pkl'
 
 with open(in_file, 'r') as f:
     data = pkl.load(f)
 
-# Printing only the first 10 entries.
-for i in data[1:10]:
+# Printing only the first 5 entries.
+for i in data[1:5]:
     print i
+    for x in range(20):
+        print "~",
+    print "~"
 
+print ""
+print "Total Entries :", len(data)
